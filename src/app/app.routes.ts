6587fd4cf24f6b8d50b68da47a004955d93789a7
ClydeH <a0937015599@gmail.com>
2025-12-08 11:07:13 +0800

    feat: add home page 與路由

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    //(path:)後面路徑自己命名
+
+    { path: 'Home', component: Home },
+
+
+
+
+    { path: '**', component: Home }, //亂打網址到到這 要最下方
+    { path: '', redirectTo: '/Home', pathMatch: 'full' }, //重新倒到首頁到Home 這樣Home為初始頁  但先要設path:home
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
