2876f5bdf405d7e2f69d9cd4a0277b15b9986ee9
ClydeH <a0937015599@gmail.com>
2025-12-11 17:40:10 +0800

    feat: update brand-story 30%內容

--- src/app/pages/brand-story/brand-story.ts
+++ src/app/pages/brand-story/brand-story.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Nav } from '../nav/nav';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-brand-story',
-  imports: [],
+  imports: [Nav, Footer],
   templateUrl: './brand-story.html',
   styleUrl: './brand-story.scss',
 })
