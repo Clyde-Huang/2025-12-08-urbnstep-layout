cba7c28cf62ba2041fd1bad037265813b29af63f
ClydeH <a0937015599@gmail.com>
2025-12-11 16:33:04 +0800

    feat: add brand-story並接好路由

---
+++ src/app/pages/brand-story/brand-story.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-story',
+  imports: [],
+  templateUrl: './brand-story.html',
+  styleUrl: './brand-story.scss',
+})
+export class BrandStory {
+
+}
